b6b86f3d220a5caad0c31951865b6a2dbf947e14
gilad <gilad.ilani@gmail.com>
2022-09-17 21:48:56 +0300

    wip

--- client/Client.cpp
+++ client/Client.cpp
@@ -1,27 +1,21 @@
 #include "Client.h"
 #include "utils.h"
+
 #include <string>
+#include <stdexcept>
 #include <boost/asio.hpp>
 #include <fstream>
 #pragma once
 
 Client::Client()
 {
-  std::vector<std::string> connection_cradentials = this->get_connection_credentials();
+
+  std::vector<std::string> connection_credentials = this->get_connection_credentials();
   boost::asio::io_context io_context;
   boost::asio::ip::tcp::socket client_socket(io_context);
   boost::asio::ip::tcp::resolver resolver(io_context);
   this->socket = &client_socket;
-  try
-  {
-    boost::asio::connect(client_socket, resolver.resolve(connection_cradentials[0], connection_cradentials[1]));
-  }
-  catch (const std::exception &e)
-  {
-    print("Failed connect to serve:");
-    print(e.what());
-    exit(-1);
-  }
+  boost::asio::connect(client_socket, resolver.resolve(connection_credentials[0], connection_credentials[1]));
 }
 Client::~Client()
 {
@@ -32,25 +26,34 @@ std::vector<std::string> Client::get_connection_credentials()
 {
   std::string address;
   std::string port;
-  const std::string transfer_path = "transfer.info";
-  try
-  {
-    std::ifstream transfer_file(transfer_path);
-    getline(transfer_file, address, ':');
-    getline(transfer_file, port, '\n');
-    transfer_file.close();
-  }
-  catch (const std::exception &e)
-  {
-    print("Failed to open transfer.info file");
-  }
+
+  std::ifstream transfer_file(TRANSFER_FILE);
+  getline(transfer_file, address, ':');
+  getline(transfer_file, port, '\n');
+  transfer_file.close();
+
   if (port.length() < 1 || address.length() < 1)
   {
-    print("transfer.info file is missing port or address");
+    throw std::invalid_argument("transfer.info file is missing port or address");
   }
   std::vector<std::string> to_ret{address, port};
   return to_ret;
 }
+bool Client::user_file_exist()
+{
+  std::string info_file_name = "me.info";
+  std::ifstream info_file(info_file_name);
+  return info_file.good();
+}
+std::string get_user_name_from_file()
+{
+  std::string buff;
+  std::string user_name;
+  std::ifstream transfer_file(TRANSFER_FILE);
+  getline(transfer_file, buff, '\n');
+  getline(transfer_file, user_name, '\n');
+}
+
 void Client::register_user()
 {
   if (this->user_file_exist())
@@ -58,13 +61,4 @@ void Client::register_user()
     print("user credentials already exist.");
     return;
   }
-  
-  
-}
-
-bool Client::user_file_exist()
-{
-  std::string info_file_name = "me.info";
-  std::ifstream info_file(info_file_name);
-  return info_file.good();
 }
--- client/Client.h
+++ client/Client.h
@@ -1,5 +1,5 @@
 #include <boost/asio.hpp>
-
+#define TRANSFER_FILE "transfer.info"
 class Client{
     public:
         Client();
@@ -7,6 +7,7 @@ class Client{
         void register_user();
     private:
         bool user_file_exist();
+        std::string get_user_name_from_file();
         boost::asio::ip::tcp::socket * socket;
         static boost::asio::ip::tcp::socket init_connection(std::string ,std::string );
         static std::vector<std::string> get_connection_credentials();
