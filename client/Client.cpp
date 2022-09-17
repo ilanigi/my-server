8c9a6d1c0e0cc186696923b73fafd9d5bbd15e65
gilad <gilad.ilani@gmail.com>
2022-09-17 20:27:58 +0300

    basic cliect is ready

--- client/Client.cpp
+++ client/Client.cpp
@@ -1,22 +1,30 @@
 #include "Client.h"
+#include "utils.h"
+#include <string>
 #include <boost/asio.hpp>
 #include <fstream>
 #pragma once
 
-
 Client::Client()
 {
   std::vector<std::string> connection_cradentials = this->get_connection_cradentilas();
-  Client::init_connection(connection_cradentials[0], connection_cradentials[1]);
-}
-
-boost::asio::ip::tcp::socket Client::init_connection(std::string address, std::string port)
-{
   boost::asio::io_context io_context;
   boost::asio::ip::tcp::socket client_socket(io_context);
   boost::asio::ip::tcp::resolver resolver(io_context);
-  boost::asio::connect(client_socket, resolver.resolve(address, port));
-  return client_socket;
+  this->socket = &client_socket;
+  try
+  {
+    boost::asio::connect(client_socket, resolver.resolve(connection_cradentials[0], connection_cradentials[1]));
+  }
+  catch (const std::exception &e)
+  {
+    print("Failed connect to serve:");
+    print(e.what());
+    exit(-1);
+  }
+}
+Client::~Client(){
+  this->socket->close();
 }
 
 std::vector<std::string> Client::get_connection_cradentilas()
@@ -27,16 +35,17 @@ std::vector<std::string> Client::get_connection_cradentilas()
   try
   {
     std::ifstream transfer_file(tranfer_path);
-    getline(transfer_file, address,':');
+    getline(transfer_file, address, ':');
     getline(transfer_file, port, '\n');
     transfer_file.close();
   }
   catch (const std::exception &e)
   {
-    // todo: add print
+    print("Failed to open tranfer.info file");
   }
-  if(port.length() < 1 || address.length()< 1){
-  
+  if (port.length() < 1 || address.length() < 1)
+  {
+    print("Failed to open tranfer.info file");
   }
   std::vector<std::string> to_ret{address, port};
   return to_ret;
--- client/Client.h
+++ client/Client.h
@@ -3,7 +3,9 @@
 class Client{
     public:
         Client();
+        ~Client();
     private:
+        boost::asio::ip::tcp::socket * socket;
         static boost::asio::ip::tcp::socket init_connection(std::string ,std::string );
         static std::vector<std::string> get_connection_cradentilas();
 };
