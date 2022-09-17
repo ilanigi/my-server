0f5fe6955df00f01da519ba610a3cc2c48bde199
gilad <gilad.ilani@gmail.com>
2022-09-17 20:48:59 +0300

    aded user_file_exist to h

--- client/Client.h
+++ client/Client.h
@@ -4,8 +4,10 @@ class Client{
     public:
         Client();
         ~Client();
+        void register_user();
     private:
+        bool user_file_exist();
         boost::asio::ip::tcp::socket * socket;
         static boost::asio::ip::tcp::socket init_connection(std::string ,std::string );
-        static std::vector<std::string> get_connection_cradentilas();
+        static std::vector<std::string> get_connection_credentials();
 };
