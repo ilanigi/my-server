082c0fc8b3e56e5146f5dbe029030908cfc396fe
ilanigi <gilad.ilani@gmail.com>
2022-10-13 16:08:46 +0300

    deleted

--- client/utils.cpp
+++
@@ -1,6 +0,0 @@
-#include <string>
-#include <iostream>
-
-void print(std::string to_print) {
-	std::cout << to_print << std::endl;
-}
--- client/utils.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-#include <string>
-void print(std::string to_print);
