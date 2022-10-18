cb787a95df25acd21fd9561036558a63f01f84b6
ilanigi <gilad.ilani@gmail.com>
2022-10-18 23:02:17 +0300

    changed names

--- server/Server.py
+++ server/Server.py
@@ -3,36 +3,37 @@ import types
 import socket
 
 HOST = "127.0.0.1"  
+MESSAGE_SIZE = 1024
 TIMEOUT = 1
 
 class Server:
     def __init__(self):
-        PORT = self.__get_port_from_file()
-        self.__init_listening(PORT)
         try:
+            PORT = self.__get_port_from_file()
+            self.__init_listening(PORT)
             self.__listen()
         except Exception as error:
             print(error)
         finally:
-            self.selector.close()
+            self.__selector.close()
 
     def __listen(self):
         while True:
-            events = self.selector.select(timeout=TIMEOUT)
+            events = self.__selector.select(timeout=TIMEOUT)
             for key, mask in events:
                 if key.data is None:
-                    self.__accept_wrapper(key.fileobj,self.selector)
+                    self.__accept_wrapper(key.fileobj)
                 else:
-                    self.__service_connection(key, mask,self.selector)
+                    self.__service_connection(key, mask)
 
     def __init_listening(self, PORT):
-        self.selector_ = selectors.DefaultSelector()
-        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server_socket.bind((HOST, PORT))
-        self.server_socket.listen()
+        self.__selector = selectors.DefaultSelector()
+        self.__server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.__server_socket.bind((HOST, PORT))
+        self.__server_socket.listen()
         print(f"server is listening on port {PORT}")
-        self.server_socket.setblocking(False)
-        self.selector.register(self.server_socket, selectors.EVENT_READ, data=None)
+        self.__server_socket.setblocking(False)
+        self.__selector.register(self.__server_socket, selectors.EVENT_READ, data=None)
 
     def __get_port_from_file(self):
         try:
@@ -44,26 +45,26 @@ class Server:
         return PORT
 
     def __accept_wrapper(self,sock):
-        conn, addr = sock.accept()  # Should be ready to read
+        conn, addr = sock.accept()  
         print(f"Accepted connection from {addr[0]}")
         conn.setblocking(False)
         data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
         events = selectors.EVENT_READ | selectors.EVENT_WRITE
-        self.selector.register(conn, events, data=data)
+        self.__selector.register(conn, events, data=data)
 
     def __service_connection(self, key, mask):
-        sock = key.fileobj
+        socket = key.fileobj
         data = key.data
         if mask & selectors.EVENT_READ:
-            recv_data = sock.recv(1024)  # Should be ready to read
+            recv_data = socket.recv(MESSAGE_SIZE)
             if recv_data:
                 data.outb += recv_data
             else:
                 print(f"Closing connection to {data.addr}")
-                self.selector.unregister(sock)
-                sock.close()
+                self.__selector.unregister(socket)
+                socket.close()
         if mask & selectors.EVENT_WRITE:
             if data.outb:
                 print(f"Echoing {data.outb!r} to {data.addr}")
-                sent = sock.send(data.outb)  # Should be ready to write
+                sent = socket.send(data.outb)  # Should be ready to write
                 data.outb = data.outb[sent:]
