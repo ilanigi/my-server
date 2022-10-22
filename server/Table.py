8bfc058abc3d5b4dd09cfde2177f39773fc0ba48
ilanigi <gilad.ilani@gmail.com>
2022-10-22 11:01:56 +0300

    insert works

--- server/Table.py
+++ server/Table.py
@@ -34,15 +34,16 @@ class Table:
     def __init_input(self,raw_columns, raw_values):
         self.__check_input(raw_columns, raw_values)
         columns = self.__list_to_string(raw_columns)
-        values = self.__list_to_string(raw_values)
+        values = self.__list_to_string(raw_values,True,raw_columns)
         return columns, values
 
 
     def create(self, raw_columns: List, raw_values: List):
         columns, values = self.__init_input(raw_columns, raw_values)
         
-        query = """INSERT INTO {table} ({columns}) VALUES ({values})""".format(
+        query = """INSERT INTO {table} ({columns}) VALUES ({values});""".format(
             table=self.__name, columns=columns, values=values)
+        
         self.__execute(query)
 
     def read():
@@ -61,17 +62,20 @@ class Table:
     def __create_new_table_query(self, columns):
         str_colomns = ""
         for column in columns:
-            str_colomns += f"{column[0]} {column[1]},"
+            str_colomns += f"{column[0]} {column[1]}, "
 
-
+        str_colomns = str_colomns[0:-2] 
         query = """CREATE TABLE {name} ({str_colomns})""".format(
             name=self.__name, str_colomns=str_colomns)
         return query
 
-    def __list_to_string(self,val_list:List):
+    def __list_to_string(self,val_list:List, values:bool = False, columns:List =[] ):
         to_ret = ""
-        
-        for val in val_list:
-            to_ret += f"{val},"
-        
-        return to_ret[0:-1]
+        for i in range(len(val_list)):
+            # adding quotation marks to string values
+            if values and self.__types[columns[i]] == str:
+                to_ret += f"'{val_list[i]}', "
+            else:
+                to_ret += f"{val_list[i]}, "
+        to_ret = to_ret[0:-2] + " "
+        return to_ret
