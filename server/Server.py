1c7a8d756d2e28a86c97531afc4d5804b5d3e168
gilad <gilad.ilani@gmail.com>
2022-10-29 09:31:12 +0300

    added router and moved connection to new class

--- server/Server.py
+++ server/Server.py
@@ -5,11 +5,12 @@ import socket
 
 from Services import Services
 from Database import Database
+from Router import Router
 
 
 HOST = "127.0.0.1"  
 MESSAGE_SIZE = 1024
-TIMEOUT = 1
+TIMEOUT = 0
 
 class Server:
     def __init__(self,test=False):
@@ -17,35 +18,19 @@ class Server:
             PORT = self.__get_port_from_file()
             db = Database()
             self.services = Services(db)
-            
+            self.router = Router()
             if test:
                 return 
-            self.__init_listening(PORT)
+            self.__connection = Connection(PORT)
             self.__listen()
+
         except Exception as error:
+            # TODO: change to res.error(....
             print(error)
         finally:
             if not test:
-                self.__selector.close()
-
-    def __listen(self):
-        while True:
-            events = self.__selector.select(timeout=TIMEOUT)
-            for key, mask in events:
-                if key.data is None:
-                    self.__accept_wrapper(key.fileobj)
-                else:
-                    self.__service_connection(key, mask)
-
-    def __init_listening(self, PORT):
-        self.__selector = selectors.DefaultSelector()
-        self.__server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.__server_socket.bind((HOST, PORT))
-        self.__server_socket.listen()
-        print(f"server is listening on port {PORT}")
-        self.__server_socket.setblocking(False)
-        self.__selector.register(self.__server_socket, selectors.EVENT_READ, data=None)
-
+                self.__connection.selector.close()
+    
     def __get_port_from_file(self):
         try:
             port_file = open("server/port.info", "r")
@@ -55,13 +40,22 @@ class Server:
             raise Exception('port file not found')
         return PORT
 
+    def __listen(self):
+        while True:
+            events = self.__connection.selector.select(timeout=TIMEOUT)
+            for key, mask in events:
+                if key.data is None:
+                    self.__accept_wrapper(key.fileobj)
+                else:
+                    self.__service_connection(key, mask)
+
     def __accept_wrapper(self,sock):
         conn, addr = sock.accept()  
         print(f"Accepted connection from {addr[0]}")
         conn.setblocking(False)
         data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
         events = selectors.EVENT_READ | selectors.EVENT_WRITE
-        self.__selector.register(conn, events, data=data)
+        self.__connection.selector.register(conn, events, data=data)
 
     def __service_connection(self, key, mask):
         socket = key.fileobj
@@ -69,13 +63,23 @@ class Server:
         if mask & selectors.EVENT_READ:
             recv_data = socket.recv(MESSAGE_SIZE)
             if recv_data:
-                data.outb += recv_data
-            else:
-                print(f"Closing connection to {data.addr}")
-                self.__selector.unregister(socket)
-                socket.close()
+                self.router.prase_data(recv_data)
+            # else:
+                # print(f"Closing connection to {data.addr}")
+                # self.__connection.selector.unregister(socket)
+                # socket.close()
         if mask & selectors.EVENT_WRITE:
             if data.outb:
                 print(f"Echoing {data.outb!r} to {data.addr}")
                 sent = socket.send(data.outb)  # Should be ready to write
                 data.outb = data.outb[sent:]
+
+class Connection:
+    def __init__(self,PORT):
+        self.selector = selectors.DefaultSelector()
+        self.__server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.__server_socket.bind((HOST, PORT))
+        self.__server_socket.listen()
+        print(f"server is listening on port {PORT}")
+        self.__server_socket.setblocking(False)
+        self.selector.register(self.__server_socket, selectors.EVENT_READ, data=None)
