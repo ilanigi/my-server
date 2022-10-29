f8270167c34122e57a6e4ebce1b220a08fb1c183
gilad <gilad.ilani@gmail.com>
2022-10-29 17:27:42 +0300

    introducing send service

--- server/SendService.py
+++ server/SendService.py
@@ -1,4 +1,18 @@
-class send:
+from Models import RES_CODE, Response
+
+
+class Send:
+    def __init__(self) -> None:
+        self.ok = OK()
+        pass
+
+class OK:
     def __init__(self) -> None:
         pass
+
+    def register(self, user_id: str):
+        res = Response(RES_CODE.ACC_REGISTER.value, user_id)
+        return res.compiled
+
+
     
--- server/Services.py
+++ server/Services.py
@@ -1,7 +1,9 @@
 from UsersService import Users
 from SecretService import SecretService
+from SendService import Send
 
 class Services:
     def __init__(self,db):
         self.users = Users(db)
         self.secrets = SecretService()
+        self.send = Send()
