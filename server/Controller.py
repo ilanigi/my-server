fd77181a069a44d9a0e189668b5add64b17de4c7
gilad <gilad.ilani@gmail.com>
2022-10-31 22:11:45 +0200

    added send_key

--- server/Controller.py
+++ server/Controller.py
@@ -7,8 +7,20 @@ class Controller:
         pass
 
     def register(self,user_name:str):
-        if self.services.users.user_exist(user_name):
+        # TODO: add check  - max user name size is 255 chars
+        if self.services.users.user_exist_by_name(user_name):
             raise Exception('user is already exist')
             
         user_id = self.services.users.save_user_to_db(user_name)
         return self.services.send.ok.register(user_id)
+    
+    def send_key(self,user_id, public_key):
+        if not self.services.users.user_exist_by_id(user_id):
+            raise Exception('user not exist')
+
+        self.services.users.add_public_key(user_id,public_key)
+        AES_key = self.services.secrets.create_AES_key()
+        
+
+
+
