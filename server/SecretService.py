d3cfe9b40326afcb9125a704f6db8d6214ef2266
ilanigi <gilad.ilani@gmail.com>
2022-11-06 08:33:43 +0200

    AES key is 16 random bytes.

--- server/SecretService.py
+++ server/SecretService.py
@@ -1,4 +1,5 @@
-from Crypto.Cipher import AES
+from Crypto.Cipher import AES,PKCS1_OAEP
+import os
 # from crc import CrcCalculator, Crc32
 
 class SecretService:
@@ -6,16 +7,16 @@ class SecretService:
         pass
 
     def create_AES_key(self):
-        AESkey = AES.new( AES.MODE_EAX)
-        return AESkey
+        return os.urandom(16)
     
     def encrypt_AES_key(self, AES_key, public_key):
-        cipher = AES.new(AES_key, AES.MODE_EAX)
-        _ = cipher.nonce
-        encrypt_public_key, tag = cipher.decrypt_and_verify(public_key)
+        cipher_rsa = PKCS1_OAEP.new(public_key)
+        encrypt_public_key = cipher_rsa.encrypt(AES_key)
         return encrypt_public_key
 
     def decrypt_file(AESkey, file):
+         # cipher = AES.new(AES_key, AES.MODE_EAX)
+        # encrypt_public_key, _ = cipher.encrypt_and_digest(public_key)
         file = AESkey.decrypt(file)
         return file
 
