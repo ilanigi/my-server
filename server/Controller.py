2f6e801885117c93ae62b83c01d4f9674f6ac3e5
ilanigi <gilad.ilani@gmail.com>
2022-11-09 22:05:28 +0200

    added print

--- server/Controller.py
+++ server/Controller.py
@@ -21,8 +21,10 @@ class Controller:
 
         self.__services.users.add_public_key(user_id,public_key)
         AES_key = self.__services.secrets.create_AES_key()
+        print('AES_key is', AES_key.hex())
         self.__services.users.add_AES_key(user_id, AES_key)      
         encrypt_AES_key = self.__services.secrets.encrypt_AES_key(AES_key,public_key)
+        print('encrypt_AES_key is', encrypt_AES_key.hex())
         return self.__services.send.ok.send_public_key(encrypt_AES_key)
         
 
