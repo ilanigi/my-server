81167c1efd34a8e3177c1db6158ba678787d07e0
ilanigi <gilad.ilani@gmail.com>
2022-11-17 13:22:10 +0200

    wip

--- client/client/src/Secret_service.h
+++ client/client/src/Secret_service.h
@@ -1,21 +1,21 @@
 #include <rsa.h>
 #include <osrng.h> 
-#define KEY_SIZE 1024
 #define KEY_SIZE_NET 160
 
 class Secret_service {
 public: 
 		Secret_service();
 	  ~Secret_service();
-	  char * get_public_key(char* buffer, unsigned int size) const;
+	  void get_public_key(char* keyout, unsigned int length) const;
 	  std::string decrypt(const char* cipher, unsigned int length);
 	  static uint32_t check_sum(const char* [], size_t );
 private:
-	std::string get_private_key() const;
+	static const unsigned int BITS = 1024;
 	CryptoPP::AutoSeededRandomPool rng;
 	CryptoPP::RSA::PrivateKey private_key;
-	CryptoPP::RSA::PublicKey public_key;
+	void init_from_file(std::string );
+	void init_public_key();
+	std::string get_private_key() const;
 	void add_key_to_file();
-	void decode_key(std::string );
 };
 
