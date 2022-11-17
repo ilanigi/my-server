cdbc7d53989d8a418c5614d5f8b997311edfc354
ilanigi <gilad.ilani@gmail.com>
2022-11-17 13:19:57 +0200

    wip

--- client/client/src/main.cpp
+++ client/client/src/main.cpp
@@ -5,10 +5,9 @@ int BUFF_SIZE = 1024;
 
 int main() {
     Client client;
-    //register user
-    //when user not exist - should create one
-     client.register_user();
+    client.register_user();
     client.create_RSA_keys();
+
     // when user exist - should return ...
     // char req[BUFF_SIZE] = "hi";
     // boost::asio::write(client_socket, boost::asio::buffer(req,BUFF_SIZE));
