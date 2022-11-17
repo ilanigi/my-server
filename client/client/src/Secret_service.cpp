9285e34c0930517dee4f4d946d6f819c38b3ce1d
ilanigi <gilad.ilani@gmail.com>
2022-11-17 13:22:32 +0200

    changed functions name

--- client/client/src/Secret_service.cpp
+++ client/client/src/Secret_service.cpp
@@ -10,12 +10,12 @@ Secret_service::Secret_service() {
 		base_64_private_key = File_service::get_private_key();
 		if (base_64_private_key.length() > 0) {
 			std::cout << "private key found in me.info file." << std::endl;
-			decode_key(base_64_private_key);
+			init_from_file(base_64_private_key);
 		}
 		else {
 			// create and add to me.info
 			std::cout << "crating private key" << std::endl;
-			private_key.Initialize(rng, KEY_SIZE);
+			private_key.Initialize(rng, BITS);
 			add_key_to_file();
 		}
 	}
@@ -30,20 +30,19 @@ void Secret_service::add_key_to_file() {
 	std::string base_64_key = Base_64_service::encode(get_private_key());
 	File_service::add_line_to_file(USER_FILE, base_64_key);
 }
-void Secret_service::decode_key(std::string base_64_key) {
+void Secret_service::init_from_file(std::string base_64_key) {
 	std::string key = Base_64_service::decode(base_64_key);
 	CryptoPP::StringSource ss(key, true);
 	private_key.Load(ss);
 }
 
-char* Secret_service::get_public_key(char* buffer, unsigned int size) const
+void Secret_service::get_public_key(char* keyout, unsigned int length) const
 {
 	CryptoPP::RSAFunction publicKey(private_key);
-	CryptoPP::ArraySink as(reinterpret_cast<CryptoPP::byte*>(buffer), size);
+	CryptoPP::ArraySink as(reinterpret_cast<CryptoPP::byte*>(keyout), length);
 	publicKey.Save(as);
-	return buffer;
+	return;
 }
-
 
 std::string Secret_service::get_private_key() const {
 	std::string key;
@@ -55,7 +54,7 @@ std::string Secret_service::get_private_key() const {
 std::string Secret_service::decrypt(const char* cipher, unsigned int length) {
 	std::string decrypted;
 	CryptoPP::RSAES_OAEP_SHA_Decryptor d(private_key);
-	CryptoPP::StringSource ss_2(reinterpret_cast<const CryptoPP::byte*>(cipher), length, true, new CryptoPP::PK_DecryptorFilter(rng, d, new CryptoPP::StringSink(decrypted)));
+	CryptoPP::StringSource ss_cipher(reinterpret_cast<const CryptoPP::byte*>(cipher), length, true, new CryptoPP::PK_DecryptorFilter(rng, d, new CryptoPP::StringSink(decrypted)));
 	return decrypted;
 }
 
@@ -65,3 +64,10 @@ uint32_t Secret_service::check_sum(const char * file[], size_t size) {
 	return result.checksum();
 }
 
+
+std::string Secret_service::encrypt(const std::string& plain) {
+	/*std::string cipher;
+	CryptoPP::RSAES_OAEP_SHA_Encryptor e(public_key);
+	CryptoPP::StringSource ss(plain, true, new CryptoPP::PK_EncryptorFilter(rng, e, new CryptoPP::StringSink(cipher)));*/
+	return "cipher";
+}
