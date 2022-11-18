6c2795d2405eba68c7579c728a4d630b922ef180
ilanigi <gilad.ilani@gmail.com>
2022-11-18 15:43:31 +0200

    added send file

--- client/client/src/main.cpp
+++ client/client/src/main.cpp
@@ -7,7 +7,7 @@ int main() {
     Client client;
     client.register_user();
     client.create_RSA_keys();
-
+    client.send_file();
     // when user exist - should return ...
     // char req[BUFF_SIZE] = "hi";
     // boost::asio::write(client_socket, boost::asio::buffer(req,BUFF_SIZE));
