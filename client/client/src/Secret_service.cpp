91d0a6e78ce9c493f65953309f6b55756d472b11
ilanigi <gilad.ilani@gmail.com>
2022-11-18 16:06:43 +0200

    changed no init to throw new error

--- client/client/src/Secret_service.cpp
+++ client/client/src/Secret_service.cpp
@@ -7,7 +7,7 @@
 
 
 Secret_service::Secret_service() { }
-Secret_service::~Secret_service() {};
+Secret_service::~Secret_service() {}
 
 void Secret_service::init() {
 	std::string base_64_private_key;
@@ -45,8 +45,7 @@ void Secret_service::init_from_file(std::string base_64_key) {
 void Secret_service::get_public_key(char* keyout, unsigned int length) const
 {
 	if (!isInit) {
-		std::cout << INIT_MESSAGE << std::endl;
-		return; 
+		throw std::invalid_argument(INIT_MESSAGE);
 	}
 	CryptoPP::RSAFunction publicKey(private_key);
 	CryptoPP::ArraySink as(reinterpret_cast<CryptoPP::byte*>(keyout), length);
@@ -61,17 +60,15 @@ std::string Secret_service::get_private_key() const {
 	return key;
 }
 
-std::string Secret_service::decrypt_key(const char* cipher, unsigned int length, unsigned char * buffer, unsigned int buffer_size) {
+void Secret_service::decrypt_key(const char* cipher, unsigned int length, unsigned char * buffer, unsigned int buffer_size) {
 	if (!isInit) {
-		std::cout << INIT_MESSAGE << std::endl;
-		return;
+		throw std::invalid_argument(INIT_MESSAGE);
 	}
 	std::string decrypted;
 	CryptoPP::RSAES_OAEP_SHA_Decryptor d(private_key);
 	CryptoPP::StringSource ss_cipher(reinterpret_cast<const CryptoPP::byte*>(cipher), length, true, new CryptoPP::PK_DecryptorFilter(rng, d, new CryptoPP::StringSink(decrypted)));
 	int i = 0;
 	memcpy_s(buffer, buffer_size, decrypted.data(), decrypted.length());
-
 }
 
 uint32_t Secret_service::check_sum(const char * file[], size_t size) {
@@ -83,8 +80,7 @@ uint32_t Secret_service::check_sum(const char * file[], size_t size) {
 std::string Secret_service::encrypt(const char* plain, unsigned int length)
 {
 	if (!isInit) {
-		std::cout << INIT_MESSAGE << std::endl;
-		return;
+		throw std::invalid_argument(INIT_MESSAGE);
 	}
 	CryptoPP::byte iv[CryptoPP::AES::BLOCKSIZE] = { 0 };
 	CryptoPP::AES::Encryption aesEncryption(AES_key, AES_KEY_SIZE);
@@ -96,7 +92,7 @@ std::string Secret_service::encrypt(const char* plain, unsigned int length)
 	stfEncryptor.Put(reinterpret_cast<const CryptoPP::byte*>(plain), length);
 	stfEncryptor.MessageEnd();
 
-	return "cipher";
+	return cipher;
 }
 
 void Secret_service::set_AES_key(unsigned char * buffer) {
