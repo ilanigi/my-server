a0fe48a916c164a321625a1155a53506d5cac563
ilanigi <gilad.ilani@gmail.com>
2022-11-18 15:43:07 +0200

    added init message and init and sset aes key

--- client/client/src/Secret_service.cpp
+++ client/client/src/Secret_service.cpp
@@ -1,9 +1,15 @@
 #include "Secret_service.h"
 #include "File_service.h"
 #include "Converters.h"
-#include <boost/crc.hpp >
+#include <boost/crc.hpp >	
+#include <modes.h>
 
-Secret_service::Secret_service() {
+
+
+Secret_service::Secret_service() { }
+Secret_service::~Secret_service() {};
+
+void Secret_service::init() {
 	std::string base_64_private_key;
 
 	if (File_service::file_exist(USER_FILE)) {
@@ -18,18 +24,18 @@ Secret_service::Secret_service() {
 			private_key.Initialize(rng, BITS);
 			add_key_to_file();
 		}
+		isInit = true;
 	}
 	else {
-		std::cout << "User credentials  are not exist, please register before sending key." << std::endl;
+		std::cout << INIT_MESSAGE << std::endl;
 	}
-
 }
-Secret_service::~Secret_service() {};
 
 void Secret_service::add_key_to_file() {
 	std::string base_64_key = Base_64_service::encode(get_private_key());
 	File_service::add_line_to_file(USER_FILE, base_64_key);
 }
+
 void Secret_service::init_from_file(std::string base_64_key) {
 	std::string key = Base_64_service::decode(base_64_key);
 	CryptoPP::StringSource ss(key, true);
@@ -38,6 +44,10 @@ void Secret_service::init_from_file(std::string base_64_key) {
 
 void Secret_service::get_public_key(char* keyout, unsigned int length) const
 {
+	if (!isInit) {
+		std::cout << INIT_MESSAGE << std::endl;
+		return; 
+	}
 	CryptoPP::RSAFunction publicKey(private_key);
 	CryptoPP::ArraySink as(reinterpret_cast<CryptoPP::byte*>(keyout), length);
 	publicKey.Save(as);
@@ -51,11 +61,17 @@ std::string Secret_service::get_private_key() const {
 	return key;
 }
 
-std::string Secret_service::decrypt(const char* cipher, unsigned int length) {
+std::string Secret_service::decrypt_key(const char* cipher, unsigned int length, unsigned char * buffer, unsigned int buffer_size) {
+	if (!isInit) {
+		std::cout << INIT_MESSAGE << std::endl;
+		return;
+	}
 	std::string decrypted;
 	CryptoPP::RSAES_OAEP_SHA_Decryptor d(private_key);
 	CryptoPP::StringSource ss_cipher(reinterpret_cast<const CryptoPP::byte*>(cipher), length, true, new CryptoPP::PK_DecryptorFilter(rng, d, new CryptoPP::StringSink(decrypted)));
-	return decrypted;
+	int i = 0;
+	memcpy_s(buffer, buffer_size, decrypted.data(), decrypted.length());
+
 }
 
 uint32_t Secret_service::check_sum(const char * file[], size_t size) {
@@ -64,10 +80,42 @@ uint32_t Secret_service::check_sum(const char * file[], size_t size) {
 	return result.checksum();
 }
 
+std::string Secret_service::encrypt(const char* plain, unsigned int length)
+{
+	if (!isInit) {
+		std::cout << INIT_MESSAGE << std::endl;
+		return;
+	}
+	CryptoPP::byte iv[CryptoPP::AES::BLOCKSIZE] = { 0 };
+	CryptoPP::AES::Encryption aesEncryption(AES_key, AES_KEY_SIZE);
 
-std::string Secret_service::encrypt(const std::string& plain) {
-	/*std::string cipher;
-	CryptoPP::RSAES_OAEP_SHA_Encryptor e(public_key);
-	CryptoPP::StringSource ss(plain, true, new CryptoPP::PK_EncryptorFilter(rng, e, new CryptoPP::StringSink(cipher)));*/
+	CryptoPP::CBC_Mode_ExternalCipher::Encryption cbcEncryption(aesEncryption, iv);
+
+	std::string cipher;
+	CryptoPP::StreamTransformationFilter stfEncryptor(cbcEncryption, new CryptoPP::StringSink(cipher));
+	stfEncryptor.Put(reinterpret_cast<const CryptoPP::byte*>(plain), length);
+	stfEncryptor.MessageEnd();
+
 	return "cipher";
 }
+
+void Secret_service::set_AES_key(unsigned char * buffer) {
+	memcpy_s(AES_key, AES_KEY_SIZE,buffer, AES_KEY_SIZE);
+}
+
+/*
+std::string Secret_service::decrypt(const char* cipher, unsigned int length)
+{
+	CryptoPP::byte iv[CryptoPP::AES::BLOCKSIZE] = { 0 };	// for practical use iv should never be a fixed value!
+
+	CryptoPP::AES::Decryption aesDecryption(_key, DEFAULT_KEYLENGTH);
+	CryptoPP::CBC_Mode_ExternalCipher::Decryption cbcDecryption(aesDecryption, iv);
+
+	std::string decrypted;
+	CryptoPP::StreamTransformationFilter stfDecryptor(cbcDecryption, new CryptoPP::StringSink(decrypted));
+	stfDecryptor.Put(reinterpret_cast<const CryptoPP::byte*>(cipher), length);
+	stfDecryptor.MessageEnd();
+
+	return decrypted;
+}
+*/
--- client/client/src/Secret_service.h
+++ client/client/src/Secret_service.h
@@ -1,20 +1,27 @@
 #include <rsa.h>
 #include <osrng.h> 
-#define KEY_SIZE_NET 160
 
 class Secret_service {
 public: 
-		Secret_service();
-	  ~Secret_service();
-	  void get_public_key(char* keyout, unsigned int length) const;
-	  std::string decrypt(const char* cipher, unsigned int length);
+	static const unsigned int PUBLIC_KEY_SIZE_NET = 160;
+	static const unsigned int AES_KEY_SIZE = 16;
+	Secret_service();
+	~Secret_service();
+	void init();
+	  void get_public_key(char* , unsigned int ) const;
+	  std::string decrypt_key(const char* , unsigned int , unsigned char* , unsigned int );
 	  static uint32_t check_sum(const char* [], size_t );
+	  std::string encrypt(const char* plain, unsigned int length);
+	  void set_AES_key(unsigned char* buffer);
 private:
+	bool isInit = false;
+	const std::string INIT_MESSAGE = "User credentials are not exist, please register before sending key.";
+	
 	static const unsigned int BITS = 1024;
+	unsigned char AES_key[AES_KEY_SIZE] = {0};
 	CryptoPP::AutoSeededRandomPool rng;
 	CryptoPP::RSA::PrivateKey private_key;
-	void init_from_file(std::string );
-	void init_public_key();
+	void init_from_file(std::string);
 	std::string get_private_key() const;
 	void add_key_to_file();
 };
