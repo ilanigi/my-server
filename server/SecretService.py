da8bc4b551573a50ef38844ebf4e7c316073692b
ilanigi <gilad.ilani@gmail.com>
2022-11-19 08:48:08 +0200

    fixed decrypt_file handling

--- server/SecretService.py
+++ server/SecretService.py
@@ -2,13 +2,17 @@ from Crypto.Cipher import PKCS1_OAEP, AES
 from Crypto.Random import get_random_bytes 
 from Crypto.PublicKey.RSA import import_key
 from Crypto.Hash import SHA1
+from Crypto.Util.Padding import unpad
 
 class SecretService:
+    AES_KEY_SIZE = 16
+    iv = bytearray(AES_KEY_SIZE)
+    
     def __init__(self) -> None:
         pass
 
     def create_AES_key(self):
-        return get_random_bytes(16)
+        return get_random_bytes(self.AES_KEY_SIZE)
     
     def encrypt_AES_key(self, AES_key, public_key):
         compiled_public_key = import_key(public_key)
@@ -16,10 +20,9 @@ class SecretService:
         encrypt_AES_key = cipher_obj.encrypt(AES_key)
         return encrypt_AES_key
 
-    def decrypt_file(self,AES_key, file):
-        cipher = AES.new(AES_key, AES.MODE_EAX,iv=0)
-        encrypt_public_key, _ = cipher.encrypt_and_digest(file)
-            # file = AESkey.decrypt(file)
+    def decrypt_file(self,AES_key, decypted_file):
+        cipher = AES.new(AES_key, AES.MODE_CBC, self.iv)
+        file = unpad(cipher.decrypt(decypted_file), AES.block_size)
         return file
 
     def check_sum(file):
