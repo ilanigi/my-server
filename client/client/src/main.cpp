87205e7b706dae088d61585d5eeba03f4d6380d1
ilanigi <gilad.ilani@gmail.com>
2022-11-19 23:35:54 +0200

    client is statis

--- client/client/src/main.cpp
+++ client/client/src/main.cpp
@@ -2,10 +2,10 @@
 #include <boost/asio.hpp>
 
 int main() {
-    Client client;
-    client.register_user();
-    client.create_RSA_keys();
-    client.send_file();
+    
+    Client::register_user();
+    Client::create_RSA_keys();
+    Client::send_file();
     // when user exist - should return ...
     // char req[BUFF_SIZE] = "hi";
     // boost::asio::write(client_socket, boost::asio::buffer(req,BUFF_SIZE));
