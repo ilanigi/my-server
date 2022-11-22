1f8cdf41bdbc282c32a1663fbe86f2d1e1d6ab20
ilanigi <gilad.ilani@gmail.com>
2022-11-23 00:19:37 +0200

    prettipy

--- client/client/src/main.cpp
+++ client/client/src/main.cpp
@@ -6,6 +6,7 @@ int main() {
     Client::register_user();
     Client::create_RSA_keys();
     Client::send_file();
+
     // when user exist - should return ...
     // char req[BUFF_SIZE] = "hi";
     // boost::asio::write(client_socket, boost::asio::buffer(req,BUFF_SIZE));
