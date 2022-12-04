75b2c613cacd0cd9214a8e2e6e1e5708c1de17ac
ilanigi <gilad.ilani@gmail.com>
2022-12-04 14:31:06 +0200

    added ok handling

--- server/SendService.py
+++ server/SendService.py
@@ -1,4 +1,4 @@
-from Models import RES_CODE, Response
+from Models import RES_CODE, Response, ACC_REGISTER, ACC_PUBLIC_KEY, ACC_FILE, ACC_MESSAGE, REGISTER_FAILED
 
 
 class Send:
@@ -6,11 +6,20 @@ class Send:
         pass
     
     def general(self,error):
-        res = Response(400, error)
+        res = Response(500, error)
         return res.compiled
 
-    def ok(self, code, payload) -> None:
-        res = Response(code, payload)
+    def ok(self, code, payload = None) -> None:
+        if code == RES_CODE.ACC_REGISTER.value:
+            res = ACC_REGISTER(payload) 
+        elif code == RES_CODE.ACC_PUBLIC_KEY.value:
+            res = ACC_PUBLIC_KEY(*payload)
+        elif code == RES_CODE.ACC_FILE.value:
+            res = ACC_FILE(*payload)
+        elif code == RES_CODE.ACC_MESSAGE.value:
+            res = ACC_MESSAGE()
+        elif code == RES_CODE.REGISTER_FAILED.value:
+            res = REGISTER_FAILED()
         return res.compiled   
 
 
