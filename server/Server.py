e49da83a2b3b4d448a7cb218da1c20eeb6f3592e
ilanigi <gilad.ilani@gmail.com>
2022-12-04 14:30:57 +0200

    fixed get_full_encrypted_file

--- server/Server.py
+++ server/Server.py
@@ -5,7 +5,7 @@ from Services import Services
 from Database import Database
 from struct import unpack_from
 
-from Models import HEADER_SIZE, REQ_CODE, Request_Header
+from Models import HEADER_SIZE, NAME_SIZE, REQ_CODE, Request_Header, INT_SIZE
 from Controller import Controller
 from Connection import Connection
 
@@ -14,7 +14,6 @@ from Connection import Connection
 MESSAGE_SIZE = 1024
 TIMEOUT = 0
 CLIENT_ID_SIZE = 16
-NAME_SIZE = 255
 PUBLIC_KEY_SIZE = 160
 
 class Server:
@@ -81,7 +80,7 @@ class Server:
                 
             elif header.code == REQ_CODE.SEND_PUBLIC_KEY.value:
                 format = f'<{NAME_SIZE}s{PUBLIC_KEY_SIZE}s'
-                user_name, public_key = unpack_from(format,buffer=message,offset=HEADER_SIZE)
+                _user_name, public_key = unpack_from(format,buffer=message,offset=HEADER_SIZE)
 
                 res = self.controller.send_key(header.client_id, public_key)
 
@@ -90,7 +89,7 @@ class Server:
                 _client_id, file_size, file_name = unpack_from(format,buffer=message,offset=HEADER_SIZE)
                 encrypted_file = self.get_full_encrypted_file(message)
                 
-                res = self.controller.receive_file(header.client_id, encrypted_file,file_name,file_size)
+                res = self.controller.receive_file(header.client_id, encrypted_file, file_name.decode('utf-8').rstrip('\x00') )
 
             elif header.code == REQ_CODE.CRC_FAILED.value:
                 pass
@@ -102,11 +101,13 @@ class Server:
                 raise Exception('Invalid code error')
             return res
         except Exception as error:
-            res = self.services.send.failed.general(error)
+            print(error)
+            return
 
     def get_full_encrypted_file(self, message):
-        encrypted_file = message[HEADER_SIZE:]
-        if len(message) < MESSAGE_SIZE:
+        file_content_start = HEADER_SIZE + NAME_SIZE + CLIENT_ID_SIZE + INT_SIZE
+        encrypted_file = message[file_content_start:]
+        if len(message) == MESSAGE_SIZE:
             message_suffix = self.__get_message_file()
             encrypted_file += message_suffix
         return encrypted_file
@@ -114,7 +115,7 @@ class Server:
     def __get_message_file(self):
             message = b''
             while True:
-                part = self.sock.recv(MESSAGE_SIZE)
+                part = self.socket.recv(MESSAGE_SIZE)
                 message += part
                 if len(part) < MESSAGE_SIZE:
                     break
