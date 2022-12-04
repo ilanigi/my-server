7b1ca2592f2d4b6549180b1b4965e0b6f5dd1cb5
ilanigi <gilad.ilani@gmail.com>
2022-12-04 10:17:14 +0200

    changed res handling

--- server/Controller.py
+++ server/Controller.py
@@ -1,6 +1,6 @@
 from Services import Services
 import uuid
-
+from Models import RES_CODE
 
 class Controller:
     def __init__(self, services:Services) -> None:
@@ -15,29 +15,40 @@ class Controller:
         print('client id is:',client_id.hex())
         self.__services.users.save_client_to_db(user_name, client_id)
         print("Registered user {} successfully".format(user_name))
-        return self.__services.send.ok.register(client_id)
+        return self.__services.send.ok(RES_CODE.ACC_REGISTER.value, client_id)
     
     def send_key(self,user_id, public_key):
         if not self.__services.users.client_exist_by_id(user_id):
             raise Exception('user not exist')
+        
         print('public_key is', public_key.hex())
+        
         self.__services.users.add_public_key(user_id,public_key)
         AES_key = self.__services.secrets.create_AES_key()
+        
         print('AES_key is', AES_key.hex())
+        
         self.__services.users.add_AES_key(user_id, AES_key)      
         encrypt_AES_key = self.__services.secrets.encrypt_AES_key(AES_key,public_key)
+        
         print('encrypt_AES_key is', encrypt_AES_key.hex())
         print("key exchanges successfully")
-        return self.__services.send.ok.send_public_key(encrypt_AES_key)
+
+        return self.__services.send.ok(RES_CODE.ACC_PUBLIC_KEY.value,encrypt_AES_key)
     
-    def recive_file(self,client_id:bytes, decypted_file:bytes,file_name:str):
+    def receive_file(self,client_id:bytes, encrypted_file:bytes,file_name:str):
         if not self.__services.users.client_exist_by_id(client_id):
             raise Exception('user not exist')
 
         AES_key = self.__services.users.get_AES_key(client_id)
-        file = self.__services.secrets.decrypt_file(AES_key,decypted_file)
-
+        file = self.__services.secrets.decrypt_file(AES_key,encrypted_file)
+        
+    
         self.__services.files.add_file(file,client_id,file)
+
+    def verify_file(self, file_name, client_id):
+
+        self.__services.files.set_verify(file_name, client_id, True)
 
 
 
--- server/SendService.py
+++ server/SendService.py
@@ -3,27 +3,15 @@ from Models import RES_CODE, Response
 
 class Send:
     def __init__(self) -> None:
-        self.ok = OK()
-        self.failed = Failed()
-
-class Failed:
-    def __init__(self) -> None:
         pass
     
     def general(self,error):
         res = Response(400, error)
         return res.compiled
 
-class OK:
-    def __init__(self) -> None:
-        pass
+    def ok(self, code, payload) -> None:
+        res = Response(code, payload)
+        return res.compiled   
 
-    def register(self, user_id: str):
-        res = Response(RES_CODE.ACC_REGISTER.value, user_id)
-        return res.compiled
-
-    def send_public_key(self, encrypt_public_key):
-        res = Response(RES_CODE.ACC_PUBLIC_KEY.value, encrypt_public_key)
-        return res.compiled
 
     
