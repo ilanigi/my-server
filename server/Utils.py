9d9fe7f097f3f28b0a6b8af49f4924835f5c309a
ilanigi <gilad.ilani@gmail.com>
2022-12-04 14:30:34 +0200

    fixed wrong implementation

--- server/Utils.py
+++ server/Utils.py
@@ -7,9 +7,9 @@ def lists_to_string_with_q_mark(raw_columns: List, values:bool=False):
             if values:
                 to_ret += f"{raw_columns[i]}=?, "
             else:
-                to_ret += f"{raw_columns[i]}=? AND"
+                to_ret += f"{raw_columns[i]}=? AND "
 
-        to_ret = to_ret[0:-2] if values else to_ret[0:-4]
+        to_ret = to_ret[0:-2] if values else to_ret[0:-5]
         return to_ret    
 
 def list_to_comma_string( val_list: List, values: bool = False):
